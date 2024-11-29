4695690a41c3eef0c72c2f8d33ed28ed92c621cc
Rémy Maillard <remy85603@gmail.com>
2024-11-29 17:20:37 +0100

    MEDIUM: ci: Created flight getters and setters

--- mini_project1/src/main/java/org/isep/airlineManagment/Flight.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Flight.java
@@ -4,19 +4,79 @@ import java.time.LocalDateTime;
 
 public class Flight {
     private int flightNumber;
-    private String origin;
-    private String destination;
+    private Airport origin;
+    private Airport destination;
     private LocalDateTime departureTime;
     private LocalDateTime arrivalDateTime;
     private String status;
 
 
-    public Flight(int flightNumber, String origin, String destination, LocalDateTime departure, LocalDateTime arrival, String status) {
+    public Flight(int flightNumber, Airport origin, Airport destination, LocalDateTime departure, LocalDateTime arrival, String status) {
         this.flightNumber = flightNumber;
         this.origin = origin;
         this.destination = destination;
         this.departureTime = departure;
         this.arrivalDateTime = arrival;
+        this.status = status;
+    }
+
+
+    public int getFlightNumber() {
+        return this.flightNumber;
+    }
+
+
+    public Airport getOrigin() {
+        return this.origin;
+    }
+
+
+    public Airport getdestination() {
+        return this.destination;
+    }
+
+
+    public LocalDateTime getDepartureTime() {
+        return this.departureTime;
+    }
+
+
+    public LocalDateTime getArrivalDateTime() {
+        return this.arrivalDateTime;
+    }
+
+
+    public String getStatus() {
+        return this.status;
+    }
+
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+
+    public void setOrigin(Airport origin) {
+        this.origin = origin;
+    }
+
+
+    public void setDestination(Airport destination) {
+        this.destination = destination;
+    }
+
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+
+    public void setArrivalDateTime(LocalDateTime arrivalDateTime) {
+        this.arrivalDateTime = arrivalDateTime;
+    }
+
+
+    public void setStatus(String status) {
         this.status = status;
     }
 }
