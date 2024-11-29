92ff8723871e581ef415a5443d3bf6eece3e4d56
KoRIOz675 <thomas@mafille.net>
2024-11-29 17:16:32 +0100

    MEDIUM: ci: Created employee getters and setters

--- mini_project1/src/main/java/org/isep/airlineManagment/Employee.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Employee.java
@@ -6,14 +6,35 @@ public class Employee extends Person {
     protected int numberEmployee;
     protected LocalDateTime hiringDate;
 
+
     public Employee(String name, String address, String contact, int numberEmployee, LocalDateTime hiringDate) {
         super(name, address, contact);
         this.hiringDate = hiringDate;
         this.numberEmployee = numberEmployee;
     }
 
+
     public String getRole() {
         return "role";
     }
 
+
+    public int getNumberEmployee() {
+        return numberEmployee;
+    }
+
+
+    public LocalDateTime getHiringDate() {
+        return hiringDate;
+    }
+
+
+    public void setNumberEmployee(int numberEmployee) {
+        this.numberEmployee = numberEmployee;
+    }
+
+
+    public void setHiringDate(LocalDateTime hiringDate) {
+        this.hiringDate = hiringDate;
+    }
 }
