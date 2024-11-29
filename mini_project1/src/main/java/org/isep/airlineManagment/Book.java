f03df627cef1b57b20e3fae107f4972253852ad6
Rémy Maillard <remy85603@gmail.com>
2024-11-29 16:42:59 +0100

    MEDIUM: ci: Created book  class constructor

--- mini_project1/src/main/java/org/isep/airlineManagment/Book.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Book.java
@@ -1,5 +1,16 @@
 package org.isep.airlineManagment;
 
-public class Book {
+import java.util.Date;
 
+public class Book {
+    private int reservationNumber;
+    private Date reservationDate;
+    private String status;
+
+
+    public Book(int reservationNumber, Date reservationDate, String status) {
+        this.reservationNumber = reservationNumber;
+        this.reservationDate = reservationDate;
+        this.status = status;
+    }
 }
