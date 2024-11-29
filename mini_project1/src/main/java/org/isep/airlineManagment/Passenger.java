acbd06e20e9ccdc1a0bb6f718fab90def520689d
KoRIOz675 <thomas@mafille.net>
2024-11-29 16:32:42 +0100

    MEDIUM: ci: Created passenger class constructor

--- mini_project1/src/main/java/org/isep/airlineManagment/Passenger.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Passenger.java
@@ -1,4 +1,24 @@
 package org.isep.airlineManagment;
 
-public class Passenger {
+public class Passenger extends Person{
+
+    String passport;
+    Book reservationNumber;
+
+    public Passenger(String name, String address, String contact, String passport) {
+        super(name, address, contact);
+        this.passport = passport;
+    }
+
+    public void bookFlight(){
+
+    }
+
+    public void cancelFlight(Book reservationNumber){
+
+    }
+
+    public void getReservations(Book reservationNumber){
+
+    }
 }
