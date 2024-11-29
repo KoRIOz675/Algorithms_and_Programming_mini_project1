fefa870af6a095510c513021c25ada02c3d44540
Rémy Maillard <remy85603@gmail.com>
2024-11-29 16:30:50 +0100

    MEDIUM: ci: Created flight class constructor

--- mini_project1/src/main/java/org/isep/airlineManagment/Flight.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Flight.java
@@ -1,5 +1,22 @@
 package org.isep.airlineManagment;
 
-public class Flight {
+import java.util.Date;
 
+public class Flight {
+    private int flightNumber;
+    private String origin;
+    private String destination;
+    private Date departureTime;
+    private Date arrivalDateTime;
+    private String status;
+
+
+    public Flight(int flightNumber, String origin, String destination, Date departure, Date arrival, String status) {
+        this.flightNumber = flightNumber;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureTime = departure;
+        this.arrivalDateTime = arrival;
+        this.status = status;
+    }
 }
