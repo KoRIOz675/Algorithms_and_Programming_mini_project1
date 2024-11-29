ec499afda0d01c5a4f0806408409390825f49d85
KoRIOz675 <thomas@mafille.net>
2024-11-29 17:25:57 +0100

    MEDIUM: ci: Created passenger getters and setters

--- mini_project1/src/main/java/org/isep/airlineManagment/Passenger.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Passenger.java
@@ -1,24 +1,36 @@
 package org.isep.airlineManagment;
 
 public class Passenger extends Person {
-
     private String passport;
-    protected Book reservationNumber;
+
 
     public Passenger(String name, String address, String contact, String passport) {
         super(name, address, contact);
         this.passport = passport;
     }
 
+
     public void bookFlight() {
 
     }
 
-    public void cancelFlight(Book reservationNumber) {
+
+    public void cancelFlight() {
 
     }
 
-    public void getReservations(Book reservationNumber) {
 
+    public void getReservations(int id) {
+
+    }
+
+
+    public String getPassport() {
+        return passport;
+    }
+
+
+    public void setPassport(String passport) {
+        this.passport = passport;
     }
 }
