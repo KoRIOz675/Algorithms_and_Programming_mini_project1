cb6885628b625a169750ef7208aadcdfc990a311
Rémy Maillard <remy85603@gmail.com>
2024-11-29 16:23:04 +0100

    MEDIUM: ci: Created aircraft class constructor

--- mini_project1/src/main/java/org/isep/airlineManagment/Aircraft.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Aircraft.java
@@ -1,5 +1,17 @@
 package org.isep.airlineManagment;
 
-public class Aircraft {
+import java.util.Random;
 
+public class Aircraft {
+    private int registration;
+    private String model;
+    private int capacity;
+
+
+    public Aircraft(String model, int capacity) {
+        Random random = new Random();
+        this.registration = random.nextInt(999999);
+        this.model = model;
+        this.capacity = capacity;
+    }
 }
