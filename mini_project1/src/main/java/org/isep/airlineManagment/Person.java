174b3b97312548edfa0a013bf0553e720eaa7a59
KoRIOz675 <thomas@mafille.net>
2024-11-29 17:11:02 +0100

    MEDIUM: ci: Created person getters and setters

--- mini_project1/src/main/java/org/isep/airlineManagment/Person.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Person.java
@@ -8,6 +8,7 @@ public class Person {
     protected String address;
     protected String contact;
 
+
     public Person(String name, String address, String contact) {
         Random random = new Random();
         this.id = random.nextInt(999999);
@@ -16,10 +17,35 @@ public class Person {
         this.contact = contact;
     }
 
+
     public void getInfos() {
         System.out.println("Name : "+ name);
         System.out.println("ID : "+ id);
         System.out.println("Address : "+ address);
         System.out.println("Contact : "+ contact);
     }
+
+
+    public int getId() {return this.id;}
+
+
+    public String getName() {return this.name;}
+
+
+    public String getAddress() {return this.address;}
+
+
+    public String getContact() {return this.contact;}
+
+
+    public void setId(int id) {this.id = id;}
+
+
+    public void setName(String name) {this.name = name;}
+
+
+    public void setAddress(String address) {this.address = address;}
+
+
+    public void setContact(String contact) {this.contact = contact;}
 }
