3a5cf37353e8875dfb7250fe3447bf06fa847c3b
KoRIOz675 <thomas@mafille.net>
2024-11-29 16:44:31 +0100

    MEDIUM: ci: Created AirlinePilot class constructor

--- mini_project1/src/main/java/org/isep/airlineManagment/AirlinePilot.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/AirlinePilot.java
@@ -1,4 +1,21 @@
 package org.isep.airlineManagment;
 
-public class AirlinePilot {
+import java.util.Date;
+
+public class AirlinePilot extends Employee{
+
+    String licence;
+    int flightHours;
+
+    public AirlinePilot(String name, String address, String contact, int numberEmployee, Date hiringDate, String licence, int flightHours) {
+        super(name, address, contact, numberEmployee, hiringDate);
+        this.flightHours = flightHours;
+        this.licence = licence;
+    }
+
+    public void assignFlight(){
+    }
+
+    public void getFlight(){
+    }
 }
