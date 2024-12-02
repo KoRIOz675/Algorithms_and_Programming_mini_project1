2ea460635332bba927dc4020e8c1047be5f264e0
KoRIOz675 <thomas@mafille.net>
2024-12-02 19:56:15 +0100

    MINOR: ci: Added book list for passenger class

--- mini_project1/src/main/java/org/isep/airlineManagment/Passenger.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Passenger.java
@@ -1,29 +1,29 @@
 package org.isep.airlineManagment;
 
-import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Passenger extends Person {
     private String passport;
+    private List<Book> books;
 
 
-    public Passenger(int id, String name, String address, String contact, String passport) {
+    public Passenger(int id, String name, String address, String contact, String passport, List<Book> books) {
         super(id, name, address, contact);
         this.passport = passport;
+        this.books = new ArrayList<>();
     }
 
 
     public void bookFlight() {
-
     }
 
 
     public void cancelFlight() {
-
     }
 
 
-    public void getReservations(int id) {
-
+    public void getReservations() {
     }
 
 
@@ -44,7 +44,17 @@ public abstract class Passenger extends Person {
     }
 
 
+    public List<Book> getBook() {
+        return this.books;
+    }
+
+
     public void setPassport(String passport) {
         this.passport = passport;
+    }
+
+
+    public void setBook(List<Book> book) {
+        this.books = book;
     }
 }
