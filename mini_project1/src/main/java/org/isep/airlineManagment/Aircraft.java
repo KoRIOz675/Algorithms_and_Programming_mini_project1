985b06de96747c236d09e595b5b331b97879f575
Rémy Maillard <remy85603@gmail.com>
2024-12-02 20:05:06 +0100

    MAJOR: ci: Added aircraft assign functions

--- mini_project1/src/main/java/org/isep/airlineManagment/Aircraft.java
+++ mini_project1/src/main/java/org/isep/airlineManagment/Aircraft.java
@@ -1,15 +1,46 @@
 package org.isep.airlineManagment;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
 public class Aircraft {
     private String registration;
     private String model;
     private int capacity;
+    private HashMap<LocalDateTime, LocalDateTime> timeTable;
 
 
     public Aircraft(String registration, String model, int capacity) {
         this.registration = registration;
         this.model = model;
         this.capacity = capacity;
+        this.timeTable = new HashMap<LocalDateTime, LocalDateTime>();
+    }
+
+
+    public Boolean assignFlight(Flight newFlight) {
+        if (this.checkAvailability(newFlight.getDepartureTime(), newFlight.getArrivalDateTime())) {
+            this.timeTable.put(newFlight.getDepartureTime(), newFlight.getArrivalDateTime());
+            return true;
+        }
+        return false;
+    }
+
+
+    public Boolean checkAvailability(LocalDateTime departureTime, LocalDateTime arrivalTime) {
+        for (LocalDateTime departureDate : this.timeTable.keySet()) {
+            if (departureTime.isAfter(departureDate)) {
+                if (departureTime.isBefore(this.timeTable.get(departureDate))) {
+                    return false;
+                }
+            };
+            if (arrivalTime.isBefore(this.timeTable.get(departureDate))) {
+                if (arrivalTime.isAfter(departureDate)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 
